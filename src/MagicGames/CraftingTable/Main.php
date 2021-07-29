4399004b33b542f8cd04690891606202c7caba47
Pushkar Singh <84075300+llPushkarSinghll@users.noreply.github.com>
2021-07-29 11:43:31 +0530

    update Main.php

--- src/MagicGames/CraftingTable/Main.php
+++ src/MagicGames/CraftingTable/Main.php
@@ -4,6 +4,22 @@ namespace MagicGames\CraftingTable;
 
 use pocketmine\plugin\PluginBase;
 
+use pocketmine\event\Listener;
+
+use pocketmine\utils\TextFormat;
+
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacketV2;
+use pocketmine\level\sound\AnvilUseSound;
+use pocketmine\level\sound\PopSound;
+
+use pocketmine\item\Item;
+use pocketmine\item\Tool;
+use pocketmine\item\Armor;
+
+use pocketmine\Server;
+use pocketmine\Player;
+
 class Main extends PluginBase {
 
   public function onEnable(){
